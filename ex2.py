af2c6e0b49df7d546ffa38fe75bf48bdd3a4fcd1
Takemi Asakura <tasakura@redhat.com>
2023-07-12 20:54:40 +0900

    Initial commit

--- ex2.py
+++ ex2.py
@@ -13,17 +13,42 @@
 # 1. Make a python list of the 5 items above and print it out.
 
 # INSERT YOUR CODE HERE
+shopping_list = [
+    {"Apples": 2.99},
+    {"Milk": 1.79},
+    {"Bread": 3.49},
+    {"Chicken": 6.99},
+    {"Pasta": 2.49}
+]
 
 # 2. Use python as your calculator and print out the total cost of
 #    all items on shopping list.
 
 # INSERT YOUR CODE HERE
+shopping_sum = shopping_list[0]["Apples"] + \
+               shopping_list[1]["Milk"] + \
+               shopping_list[2]["Bread"] + \
+               shopping_list[3]["Chicken"] + \
+               shopping_list[4]["Pasta"]
+
+print(round(shopping_sum,2))
 
 # 3. I have decided that we need 5 cartons of milk, can you recalculate
 #    it and print it out again?
 
 # INSERT YOUR CODE HERE
+shopping_sum = shopping_list[0]["Apples"] + \
+               shopping_list[1]["Milk"]*5 + \
+               shopping_list[2]["Bread"] + \
+               shopping_list[3]["Chicken"] + \
+               shopping_list[4]["Pasta"]
+               
+print(round(shopping_sum,2))
 
 # 4. Print out every item of your shopping list on a new line.
 
 # INSERT YOUR CODE HERE
+for item in shopping_list:
+    shopping_item = list(item.keys())[0]
+    print(shopping_item)
+
